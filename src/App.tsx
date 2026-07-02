996f49a2db6525230ba0ef2d41c4fdae4d1c8fc8
DanielPini <daniel@danielpini.com>
2026-07-02 17:05:15 +0200

    feat(SelectedWorks): extracted to own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import SelectedWorks from "./components/SelectedWorks";
+
 export default function App() {
-  return <h1>Hello birbs</h1>;
+  return (
+    <>
+      <h1>Hello birbs</h1>
+      <SelectedWorks />
+    </>
+  );
 }
---
+++ src/components/SelectedWorks.tsx
@@ -0,0 +1,24 @@
+export default function SelectedWorks() {
+  return (
+    <>
+      <h2>Selected works</h2>
+      <ul>
+        <a href="https://coma.org">
+          <li>CoMA</li>
+        </a>
+        <a href="https://niki-johnson.com">
+          <li>Niki Johnson</li>
+        </a>
+        <a href="https://straddle-poker.com">
+          <li>Straddle Poker</li>
+        </a>
+        <a href="https://jack-farman.netlify.app">
+          <li>Jack Farman</li>
+        </a>
+        <a href="https://earthly-futures.com">
+          <li>Earthly Futures Studio</li>
+        </a>
+      </ul>
+    </>
+  );
+}
