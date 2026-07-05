217f60d9c70043dd75169bb6ac16e1070da1ed4c
DanielPini <daniel@danielpini.com>
2026-07-05 23:43:02 +0200

    fix(vite config): update base for custom domain name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/bag-o-birds/",
+  base: "/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
